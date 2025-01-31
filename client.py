f5db021fd97e6f7813e806f37f421f7a3cdf7a59
mjbaker474 <mjbaker474@gmail.com>
2025-01-31 15:57:36 -0600

    Complete

--- client.py
+++ client.py
@@ -4,20 +4,24 @@
 # Assignment: 4 - Pipe Spike
 # Description: Client for ZeroMQ demo
 
-import time
 import zmq
 
 
-
 def main():
+    # Create a ZMQ context object and use it to connect to a socket.
     context = zmq.Context()
     print("Client attempting to connect to server...")
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:5555")
-    print("Sending a request...")
-    socket.send_string("This is a request.")
-    message = socket.recv()
-    print(f"Server sent back: {message.decode()}")
+
+    # Send a request to the server.
+    request = "This is a CS361 request."
+    print(f"Sending the following request to the server: {request}")
+    socket.send_string(request)
+
+    # Receive response from the server.
+    response = socket.recv()
+    print(f"Server replied: {response.decode()}")
     socket.send_string("Q")
 
 
--- server.py
+++ server.py
@@ -9,19 +9,25 @@ import zmq
 
 
 def main():
+    # Create a ZMQ context opject and use it to bind a socket.
     context = zmq.Context()
     socket = context.socket(zmq.REP)
     socket.bind("tcp://*:5555")
 
+    # Listen for a request from client.
     while True:
         message = socket.recv()
-        print(f"Received request from the client: {message.decode()}")
+        # Message received.
         if len(message) > 0:
+            print(f"Received request from the client: {message.decode()}")
             if message.decode() == 'Q':
                 break
             time.sleep(3)
-            socket.send_string("This is a response.")
+            response = "Wow, thank you for your CS361 message!"
+            print(f"Sending response to the client: {response}")
+            socket.send_string(response)  # Respond to message
     context.destroy()  # RIP old friend
+
 
 if __name__ == '__main__':
     main()
